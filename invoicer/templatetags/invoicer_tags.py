eb97ee5096b6b0f96cf85b9bba380d45e9d63b2e
Mario Orlandi <morlandi@brainstorm.it>
2011-11-24 19:55:27 +0100

    decimals numbers nice format with thousand separators

--- invoicer/templatetags/invoicer_tags.py
+++ invoicer/templatetags/invoicer_tags.py
@@ -1,12 +1,31 @@
 from django.template.base import Library
 from django.template.defaultfilters import stringfilter
+from django.template.defaultfilters import floatformat
+from django.utils.encoding import force_unicode
+import re
 
 register = Library()
 
-@register.filter
-def normalize_floatnumber(value):
-    """Converts a string into all lowercase."""
-    return value.replace(',','.')
-normalize_floatnumber.is_safe = True
-normalize_floatnumber = stringfilter(normalize_floatnumber)
+# from "humamize" app
+def _my_intcomma(value):
+    """
+    Converts an integer to a string containing commas every three digits.
+    For example, 3000 becomes '3,000' and 45000 becomes '45,000'.
+    """
+    orig = force_unicode(value)
+    new = re.sub("^(-?\d+)(\d{3})", '\g<1>,\g<2>', orig)
+    if orig == new:
+        return new
+    else:
+        return _my_intcomma(new)
 
+@register.filter
+def floatformatex(text, arg=-1):
+    text = floatformat(text,arg).replace(',','.')
+    tokens = text.split('.')
+    text = _my_intcomma(tokens[0])
+    if len(tokens)>1:
+        text = '.'.join([text, tokens[1],])
+    return text
+floatformatex.is_safe = True
+floatformatex = stringfilter(floatformatex)
